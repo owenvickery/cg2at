1c3589552306ac4861b4ee33c14be8c6ec3147c3
owen vickery <42470318+owenvickery@users.noreply.github.com>
2020-10-16 15:25:05 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-if [[ ! -z "${CONDA_PREFIX}" ]]; then ln -fs $PREFIX/database/script_files/cg2at.py $CONDA_PREFIX/bin/CG2AT;fi
+if [[ ! -z "${CONDA_PREFIX}" ]]; then ln -fs $PREFIX/info/recipe/database/script_files/cg2at.py $CONDA_PREFIX/bin/CG2AT;fi
