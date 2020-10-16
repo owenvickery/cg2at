56762744ad09dcf2f2c51fdb19b13862402aee35
owenvickery <owenvickery@gmail.com>
2020-10-16 14:53:11 +0100

    test sym

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-touch $PREFIX/test.dat
+ln -s $PREFIX/info/recipe/CG2AT $CONDA_PREFIX/bin/CG2AT
+#if [ -z ${CONDA_PREFIX} ]; then ln -n $PREFIX/info/recipe/CG2AT $CONDA_PREFIX/bin/CG2AT; else echo "bugger" ;fi
