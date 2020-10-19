8a1cffdd6ed4dde215ff0ab0784d32c455148787
owenvickery <owenvickery@gmail.com>
2020-10-19 14:07:37 +0100

    try catch

--- build.sh
+++ build.sh
@@ -7,11 +7,13 @@
 #  	fi
 # fi
 printenv > $CONDA_PREFIX/test_cg2at_env.txt
-if [[ ${CONDA_PREFIX} != "/home/travis/"* ]] 
-then
-	echo "ln -fs $CONDA_PREFIX/bin/$package/cg2at $CONDA_PREFIX/bin/cg2at  correct" > $CONDA_PREFIX/test_cg2at.txt
-	ln -fs $CONDA_PREFIX/bin/$package/cg2at $CONDA_PREFIX/bin/cg2at
+# if [[ ${CONDA_PREFIX} != "/home/travis/"* ]] 
+# then
+# 	echo "ln -fs $CONDA_PREFIX/bin/$package/cg2at $CONDA_PREFIX/bin/cg2at  correct" > $CONDA_PREFIX/test_cg2at.txt
+# 	ln -fs $CONDA_PREFIX/bin/$package/cg2at $CONDA_PREFIX/bin/cg2at
 
-else
-	echo "ln -fs $CONDA_PREFIX/bin/$package/cg2at $CONDA_PREFIX/bin/cg2at  wrong" > $CONDA_PREFIX/test_cg2at.txt
-fi
+# else
+# 	echo "ln -fs $CONDA_PREFIX/bin/$package/cg2at $CONDA_PREFIX/bin/cg2at  wrong" > $CONDA_PREFIX/test_cg2at.txt
+# fi
+
+{ ln -fs $CONDA_PREFIX/bin/$package/cg2at $CONDA_PREFIX/bin/cg2at && } || { echo "ln -fs $CONDA_PREFIX/bin/$package/cg2at $CONDA_PREFIX/bin/cg2at  wrong" > $CONDA_PREFIX/test_cg2at.txt }
