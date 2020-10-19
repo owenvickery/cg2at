19484d9e0d76e2293570020a14de7bb112016a9f
owenvickery <owenvickery@gmail.com>
2020-10-19 13:25:58 +0100

    added ln

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@
 #  		ln -fs $PREFIX/info/recipe/database/script_files/cg2at.py $CONDA_PREFIX/bin/CG2AT
 #  	fi
 # fi
+ln -fs $CONDA_PREFIX/bin/$package/cg2at $CONDA_PREFIX/bin/cg2at
 # echo $CONDA_PREFIX/bin/CG2AT > $CONDA_PREFIX/test_cg2at.txt
-printenv > $CONDA_PREFIX/test_cg2at.txt
+# printenv > $CONDA_PREFIX/test_cg2at.txt
 # $PATH >> $CONDA_PREFIX/test_cg2at.txt
 # echo $PREFIX/pre >> $CONDA_PREFIX/test_cg2at.txt
 # export LD_RUN_PATH = $ORIGIN
