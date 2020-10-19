52fd1443379a3241a98698b4548e4fb5b1c9e0ac
owenvickery <owenvickery@gmail.com>
2020-10-19 13:57:59 +0100

    added else

--- build.sh
+++ build.sh
@@ -6,15 +6,12 @@
 #  		ln -fs $PREFIX/info/recipe/database/script_files/cg2at.py $CONDA_PREFIX/bin/CG2AT
 #  	fi
 # fi
+printenv > $CONDA_PREFIX/test_cg2at_env.txt
 if [[ ${CONDA_PREFIX} != "/home/travis/"* ]] 
 then
 	echo "ln -fs $CONDA_PREFIX/bin/$package/cg2at $CONDA_PREFIX/bin/cg2at  correct" > $CONDA_PREFIX/test_cg2at.txt
 	ln -fs $CONDA_PREFIX/bin/$package/cg2at $CONDA_PREFIX/bin/cg2at
+
 else
 	echo "ln -fs $CONDA_PREFIX/bin/$package/cg2at $CONDA_PREFIX/bin/cg2at  wrong" > $CONDA_PREFIX/test_cg2at.txt
 fi
-# echo $CONDA_PREFIX/bin/CG2AT > $CONDA_PREFIX/test_cg2at.txt
-# printenv > $CONDA_PREFIX/test_cg2at.txt
-# $PATH >> $CONDA_PREFIX/test_cg2at.txt
-# echo $PREFIX/pre >> $CONDA_PREFIX/test_cg2at.txt
-# export LD_RUN_PATH = $ORIGIN
