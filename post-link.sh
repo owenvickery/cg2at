593eed5ece4bebb0bc7075099863a1ab55eada0f
owenvickery <owenvickery@gmail.com>
2020-10-19 14:37:37 +0100

    add post-link.sh

---
+++ post-link.sh
@@ -0,0 +1,2 @@
+ln -fs $CONDA_PREFIX/pkgs/$package/info/recipe/cg2at $CONDA_PREFIX/bin/cg2at ||  echo "ln -fs $CONDA_PREFIX/pkgs/$package/info/recipe/cg2at $CONDA_PREFIX/bin/cg2at  wrong" > $PREFIX/.messages
+
