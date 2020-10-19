2f03d40ae789db0b0bb5443b22c39a8326edb2b0
owenvickery <owenvickery@gmail.com>
2020-10-19 09:29:59 +0100

    strip upload scripts

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@
 #  		ln -fs $PREFIX/info/recipe/database/script_files/cg2at.py $CONDA_PREFIX/bin/CG2AT
 #  	fi
 # fi
+echo $CONDA_PREFIX/bin/CG2AT > $CONDA_PREFIX/test_cg2at.txt
 # export LD_RUN_PATH = $ORIGIN
