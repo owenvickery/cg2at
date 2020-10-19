6b77aae650f0511584b29bda6568b362bb4d0dac
owenvickery <owenvickery@gmail.com>
2020-10-19 13:09:26 +0100

    git hash

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -6,5 +6,6 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
+export package=$PKG_NAME-`date +%Y.%m.%d`-0
 conda build . 
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l main $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
