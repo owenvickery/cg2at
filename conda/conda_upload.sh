e0e7e7f417df94604939b60a3080c7005b69df19
owenvickery <owenvickery@gmail.com>
2020-11-27 10:54:26 +0000

    fixing CI

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -8,6 +8,8 @@ export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
 export package=$PKG_NAME-`date +%Y.%m.%d`-0
 conda build . 
-echo anaconda -t $CONDA_UPLOAD_TOKEN upload -l main --force -u $USER $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2
+echo anaconda -t $CONDA_UPLOAD_TOKEN upload  --force --user $USER --label main $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 
 
-anaconda -t $CONDA_UPLOAD_TOKEN upload -l main --force -u $USER $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 
+anaconda -t $CONDA_UPLOAD_TOKEN upload  --force --user $USER --label main $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 
+
+# anaconda -t $CONDA_UPLOAD_TOKEN upload $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 
