e1bf22c62cabaa183a124c2cf6d690bfb4e9a4c4
owen vickery <owenvickery@gmail.com>
2021-07-08 14:37:07 +0100

    Update conda_upload.sh

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -2,7 +2,7 @@
 PKG_NAME=cg2at
 USER=stansfeld_rg
 
-OS=linux-64
+OS=noarch
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
@@ -10,5 +10,6 @@ export VERSION=`date +%Y.%m.%d`
 export package=$PKG_NAME-`date +%Y.%m.%d`-0
 conda build . 
 ls -ltrh $CONDA_BLD_PATH/
-ls -ltrh $CONDA_BLD_PATH/$OS/
+ls -ltrh $CONDA_BLD_PATH/linux-64/
+ls -ltrh $CONDA_BLD_PATH/noarch/
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l main $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
